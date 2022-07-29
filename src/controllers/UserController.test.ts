45af34059b3424c254cdd2734b693010bac47fea
Glen112 <escobar.glen@gmail.com>
2022-07-29 09:44:07 -0300

    tests-jest-UserController-Object

--- src/controllers/UserController.test.ts
+++ src/controllers/UserController.test.ts
@@ -4,29 +4,34 @@ import {makeMockRequest} from '../__mocks__/mockRequest'
 import {makeMockResponse} from '../__mocks__/mockResponse'
 import {User} from '../entities/User'
 
+const mockUser: User = getMockUser()
 jest.mock('../services/UserService', ()=>{
   return{
     UserService: jest.fn().mockImplementation(()=>{
       return{
-        createUser: jest.fn()
+        createUser: jest.fn().mockImplementation(() => Promise.resolve(mockUser))
       }
     })
   }
 })
 describe('UserController', ()=>{
   const userController = new UserController()
-  const mockUser: User = getMockUser()
 
   it('Deve retornar status 201 e o usuário criado', async ()=>{
     const request = makeMockRequest({
       body:{
-        name:'some name',
-        email:'some@email.com'
+        name:'algun usuario',
+        email:'algun@email.com'
       }
     })
     const response = makeMockResponse()
     const user = await userController.createUser(request, response) 
     expect(response.state.status).toBe(201)
+    expect(response.state.json).toHaveProperty('user_id')
+    expect(response.state.json).toMatchObject({
+        name:'algun usuario',
+        email:'algun@email.com'
+      })
     })
   })
 
