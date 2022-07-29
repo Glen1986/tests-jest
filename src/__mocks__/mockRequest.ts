de554ddcad433fb8b753710fa09c968e661ee5da
Glen112 <escobar.glen@gmail.com>
2022-07-29 11:21:01 -0300

    Object-params

--- src/__mocks__/mockRequest.ts
+++ src/__mocks__/mockRequest.ts
@@ -1,12 +1,15 @@
 import { Request } from 'express'
 import { Params } from 'express-serve-static-core'
 
-export function makeMockRequest ({ params, query, body }:
-    { params?: Params, query?: Params, body?: Params }): Request {
+export function makeMockRequest ({ params, query, body, name, email }:
+  { params?: Params, query?: Params, body?: Params, name?:String, email?:String }): Request {
   const request = {
     params: params || {},
     query: query || {},
-    body: {}
+    body: {
+      name:name,
+      email:email
+    }
   } as unknown
 
   return request as Request
